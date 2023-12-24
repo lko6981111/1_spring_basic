fe520c1e813799b3e7de992dfcb9b3ca4a588599
changseok <lko6981111@gmail.com>
2023-12-25 02:27:50 +0900

    3-9

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,18 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     //이 두개는 DIP,OCP 위반
-
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
     //인터페이스에만 의존 but 구현체가 없어서 NPE(null pointer exception)발생
     //이 문제를 해결하려면 누군가가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy=discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
